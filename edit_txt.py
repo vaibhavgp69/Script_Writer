0aef650e2e67e7ba5e1de89fde684fbda40489ea
Vaibhav GP <112196928+vaibhavgp69@users.noreply.github.com>
2022-09-08 20:18:12 +0530

    Create edit_txt.py
    THIS IS TO REMOVE THE TIMESTAMP DATA WHEN YOU EXPORT TXT FILE FROM WHATSAPP

---
+++ edit_txt.py
@@ -0,0 +1,4 @@
+with open('file_name.txt', 'r', encoding='utf-8') as inFile,\
+     open('file_name.txt', 'w', encoding='utf-8') as outFile:
+    for line in inFile:
+        outFile.write(line[16:]) #do feel free to change the 17 to any number - THIS IS TO REMOVE THE TIMESTAMP DATA WHEN YOU EXPORT TXT FILE FROM WHATSAPP
