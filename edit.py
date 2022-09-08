d933ee7626ea5772954b24e64fba96fe95a2134d
Vaibhav GP <112196928+vaibhavgp69@users.noreply.github.com>
2022-09-08 21:26:02 +0530

    Add files via upload

--- edit.py
+++ edit.py
@@ -1,4 +1,4 @@
-with open('bois.txt', 'r', encoding='utf-8') as inFile,\
-     open('boisn.txt', 'w', encoding='utf-8') as outFile:
+with open('file_name.txt', 'r', encoding='utf-8') as inFile,\
+     open('file_name.txt', 'w', encoding='utf-8') as outFile:
     for line in inFile:
-        outFile.write(line[17:])
+        outFile.write(line[17:]) #do feel free to change the 17 to any number - THIS IS TO REMOVE THE TIMESTAMP DATA WHEN YOU EXPORT TXT FILE FROM WHATSAPP
